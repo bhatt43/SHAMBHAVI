49a524c88095fbbdf0cb403c49c00f085d1044b2
bhatt43 <24bit012@sot.pdpu.ac.in>
2025-04-23 11:04:19 +0530

    Add files via upload

---
+++ 1.5.py
@@ -0,0 +1,22 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+c=a=b
+print(c)
+
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+d=a-b
+print(d)
+
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+e=a*b
+print(e)
+
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+f=a/b
+print(f)
